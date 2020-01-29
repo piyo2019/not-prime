86a86b3b3b3da4dc28125519b66052f754fdfab1
piyo2019 <hakobe2001@yahoo.co.jp>
2020-01-30 02:26:26 +0900

    listを小さい順に並べる，homeに個数や最大値，総数を出すようにするなど，いろいろ修正

--- test.js
+++ test.js
@@ -2,20 +2,19 @@
 const util = require('./lib/util');
 const func= require('./lib/func');
 
-//const a=func.randomNumbersGet25(max,kosuu,npMap);
-//util.fileToMap();
+/** randomNumbersGet25np関数のチェック
 const a=util.randomNumbersGet25np();
 console.log('array='+a.n);
 console.log('maxNow='+a.maxNow);
-/** 
-const max=1000;
-const kosuu=20;
-//const npMap=new Map();
-
-
-util.fileToMap();
-const a=func.randomNumbersGet25(max,kosuu,npMap);
-console.log('npMapSize='+ util.npMapSize());
-console.log('n='+a.n);
-console.log('maxNow='+a.maxNow);
 */
+
+/**
+ * 
+ */
+util.fileToMap();
+const total= util.npMapSize();
+const maxNow=util.getMaxNow();
+const kosuu=util.getKosuu();
+console.log(`現在，${maxNow}までの数を${kosuu}個ランダムのとってきて調べています。`);
+console.log(`集まった数は全部で${total}個です`);
+
